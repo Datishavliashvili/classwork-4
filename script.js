ee84e569baedadfb1bffe4a572a7383066a0d012
Datishavliashvili <davit.shavliashvili04@gmail.com>
2025-05-21 21:46:15 +0400

    adding lecture

---
+++ script.js
@@ -0,0 +1,66 @@
+// let username = "Giorgi";
+
+// console.log(typeof username);
+
+// let age = 30;
+
+// console.log(typeof age);
+
+// let isStudent = false;
+
+// console.log(typeof isStudent);
+
+// let testOne = null;
+
+// console.log(typeof testOne);
+
+// let job;
+
+// console.log(typeof job);
+
+let UserProfile = {
+  id: 1234,
+  username: "dati",
+  isStudent: false,
+  email: "datiururu@gmail.com",
+  recemail: null,
+  laptop: "ASUS",
+  address: {
+    country: "georgia",
+    city: "tbilisi",
+  },
+};
+
+let UserProfile2 = {
+  id: 1235,
+  username: "giorgi",
+  isStudent: true,
+  email: "giorgiasd@gmail.com",
+  recemail: null,
+  laptop: "ROG",
+  address: {
+    country: "NY",
+    city: "tbilisi",
+    street: "aghmashenebeli",
+    homeid: 10,
+  },
+};
+
+let username [
+    {  username: "giorgi",}
+{  isStudent: true,}
+{  email: "giorgiasd@gmail.com",}
+
+]
+
+console.log(UserProfile2.address.country);
+
+let keyword = "address";
+
+console.log(UserProfile2[address]);
+
+// ობიქეტი//key//value
+console.log(UserProfile.username);
+console.log(UserProfile.email);
+console.log(UserProfile.id);
+console.log(UserProfile.isStudent);
